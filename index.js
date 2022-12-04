d350383e8917e4a7230e7b33b6e831a313cae1a8
Madeline Sanchez <61523145+MadSancheese@users.noreply.github.com>
2022-12-03 21:10:37 -0700

    Wrote API and CookieParser functions n stuff
    Wrote up the API to get questions and stats to appear on the front page, as well as cookies set up to show the last time that you were on the page, or if you have never been, it will show that you are having your first visit.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const expressSession = require("express-session");
 const pug = require("pug");
+const axios = require('axios');
 const {MongoClient, ObjectId} = require('mongodb');
 const url = 'mongodb+srv://user:passw0rd123@interwebdev.82cunpv.mongodb.net/test'
 const path = require('path');
@@ -7,7 +8,6 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const cookieParser = require("cookie-parser");
 const app = express();
-
 
 const client = new MongoClient(url);
 const db = client.db('users');
@@ -52,9 +52,50 @@ const checkAuth =  (req, res, next) => {
     }
 }
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    var json_code = { 'hello': 'world' };
 
-    res.render('index');
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    await axios.get('http://localhost:3000/api').then(res => {
+        const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+        console.log('Status Code:', res.status);
+        console.log('Date in Response header:', headerDate);
+        
+        json_code = res.data;
+    }).catch(err => {
+        console.log('Error: ', err.message);
+    });
+    
+    if(req.cookies.beenHereBefore == "yes"){
+        json_code = {...json_code, "lastvisit": "You were last here on: " + req.cookies.beenHereDate}
+        var date = new Date();
+        var month = months[date.getMonth()];
+        var day = date.getDate();
+        var year = date.getFullYear();
+
+        date = month + ", " + day + ", " + year;
+
+        console.log(date);
+        res.cookie("beenHereDate", date, {maxAge: 999999999999});
+    }else{
+        json_code = {...json_code, "lastvisit": "Looks like it's your first time here!"}
+        
+        res.cookie("beenHereBefore", "yes", {maxAge: 999999999999});
+        var date = new Date();
+        var month = months[date.getMonth()];
+        var day = date.getDate();
+        var year = date.getFullYear();
+
+        date = month + ", " + day + ", " + year;
+
+        console.log(date);
+        res.cookie("beenHereDate", date, {maxAge: 999999999999});
+    }
+
+    console.log(json_code);
+    
+    res.render('index', { title: 'Express', data: JSON.stringify(json_code)}); 
 });
 
 app.get("/login", (req, res) => {
@@ -78,7 +119,6 @@ app.post("/login", urlencodedParser, async (req, res) => {
             isAuthenticated : true,
             id: user._id
         }
-        //console.log('id:', req.session.user._id);
         
         res.redirect('/edit/' + user._id);
     }
@@ -113,7 +153,6 @@ app.post("/postRegister", urlencodedParser, async (req, res) => {
     if(username != null && password != null && email != null && age != null && confirm != null) {
         if(password == confirm) {
             if(userRegex.test(username) && emailRegex.test(email) && passwordRegex.test(password) && !isNaN(age)) {
-
                 await client.connect();
                 const encrypted = await encrypt(password);
                 console.log('password: ', encrypted);
@@ -129,11 +168,14 @@ app.post("/postRegister", urlencodedParser, async (req, res) => {
                 await collection.insertOne(userInfo);
                 client.close();
                 res.redirect('/login')
+            }else{
+                res.redirect("register")
             }
+        }else{
+            res.redirect("register")
         }
-    }
-    else {
-        //send them to an error page
+    }else{
+        res.redirect("register")
     }
 });
 
@@ -176,6 +218,104 @@ app.get('/logout', (req, res) => {
             res.redirect('/login');
         }
     });
+});
+
+app.get("/api", async (req, res) => {
+    await client.connect();
+
+    var users = await collection.find().toArray();
+
+    var question1_1st = 0;
+    var question1_2nd = 0;
+    var question1_3rd = 0;
+    var question1_4th = 0;
+
+    var question2_1st = 0;
+    var question2_2nd = 0;
+    var question2_3rd = 0;
+    var question2_4th = 0;
+
+    var question3_1st = 0;
+    var question3_2nd = 0;
+    var question3_3rd = 0;
+    var question3_4th = 0;
+
+    for(u in users){
+        switch(users[u].question1){
+            case "Dragon":
+                question1_1st++;
+                break;
+            case "Rabbit":
+                question1_2nd++;
+                break;
+            case "Dog":
+                question1_3rd++;
+                break;
+            case "Bull":
+                question1_4th++;
+                break;
+        }
+
+        switch(users[u].question2){
+            case "1":
+                question2_1st++;
+                break;
+            case "2":
+                question2_2nd++;
+                break;
+            case "3":
+                question2_3rd++;
+                break;
+            case "4":
+                question2_4th++;
+                break;
+        }
+
+        switch(users[u].question3){
+            case "Green, Purple, Orange":
+                question3_1st++;
+                break;
+            case "Red, Purple, Blue":
+                question3_2nd++;
+                break;
+            case "Blue, Red, Green":
+                question3_3rd++;
+                break;
+            case "Pink, Blue, Green":
+                question3_4th++;
+                break;
+        }
+    }
+
+    json = {
+        question1: {
+            title: "What animal does not appear on the Chinese Zodiac?",
+            firstresp: "Dragon: " + question1_1st,
+            secondresp: "Rabbit: " + question1_2nd,
+            thirdresp: "Dog: " + question1_3rd,
+            fourthresp: "Bull: " + question1_4th
+        },
+
+        question2: {
+            title: "How many hearts does an octopus have?",
+            firstresp: "One: " + question2_1st,
+            secondresp: "Two: " + question2_2nd,
+            thirdresp: "Three: " + question2_3rd,
+            fourthresp: "Four: " + question2_4th
+        },
+
+        question3: {
+            title: "What are the 3 distinct colors of the powerpuff girls?",
+            firstresp: "Green, purple, and orange: " + question3_1st,
+            secondresp: "Red, purple, and blue: " + question3_2nd,
+            thirdresp: "Blue, red, and green: " + question3_3rd,
+            fourthresp: "Pink, blue, and green: " + question3_4th
+        },
+
+        total: users.length
+    }
+
+    res.json(json);
 });
 
 app.listen(3000);
